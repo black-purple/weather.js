120043255339217663b954a0a240c31562a20e5f
Abderrahman DAKIR ALLAH <82895430+black-purple@users.noreply.github.com>
2022-08-28 01:33:53 +0100

    UI functional + getting geolocation

---
+++ public/index.js
@@ -0,0 +1,20 @@
+const themeButton = document.querySelector('#theme');
+let dark = true;
+themeButton.onclick = () => {
+    themeButton.firstElementChild.setAttribute('class', dark ? "bi bi-moon-fill text-info" : "bi bi-brightness-high-fill text-warning");
+    document.body.setAttribute('class', dark ? "bg-light text-dark" : "bg-dark text-light");
+    dark = !dark;
+    themeButton.setAttribute('class', dark ? "btn btn-dark p-3 m-3 text-center border-1 border-secondary" : "btn btn-light p-3 m-3 text-center border-1 border-secondary");
+}
+const searchButton = document.querySelector('#search')
+searchButton.onclick = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            window.location.href = `lon=${position.coords.longitude}&lat=${position.coords.latitude}`;
+        }, err => {
+            console.log(err);
+        });
+    } else {
+        alert('Geolocation is either blocked or not supported by this browser.');
+    }
+}
