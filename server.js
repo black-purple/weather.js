6d3105f6ae9ddea5e5018cad77cb4372d7d53338
Abderrahman DAKIR ALLAH <82895430+black-purple@users.noreply.github.com>
2022-08-29 21:26:11 +0100

    removed env.json

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
-import fs from 'fs';
-const  {apiKey} = JSON.parse(fs.readFileSync('./env.json'));
-import express from 'express';
+import {express as app} from 'express';
 import fetch from 'node-fetch';
-
-const app = express();
 
 app.use(express.static('public'));
 app.set('view-engine', 'ejs');
@@ -12,10 +8,12 @@ app.get('/', (req, res) => {
     res.render('index.ejs')
 });
 
+let apiKey = '' //* Paste your API key here before starting the app
+
 app.get('/lon=:longitude&lat=:latitude', (req, res) => {
     let longitude = req.params.longitude
     let latitude = req.params.latitude;
-    let apiLink = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey.testing}`;
+    let apiLink = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
     fetch(apiLink)
         .then(result => result.json())
         .then(json => {
